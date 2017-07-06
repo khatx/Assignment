d9b84b75df08526e640181e485839d4f5a747f35
Micheal Omar <michealcsc1996@gmail.com>
2017-07-06 20:06:09 +0300

    avoiding smelly code

--- assignment.js
+++ assignment.js
@@ -1,10 +1,6 @@
-function add(a , b)
-{
+function add(a , b){
     return a + b;
 }
-console.log (add(2 , 3));
-function subtract(x , y)
-{
+function subtract(x , y){
     return x - y;
 }
-console.log (subttract(2 , 3));
