8a9c52c04d3e2cc371602bf6e0de4c00d69d4758
Mutesasira Albert <katxalbert@gmail.com>
2017-07-07 05:13:38 -0700

    i have added arrays

--- assignment.js
+++ assignment.js
@@ -13,3 +13,12 @@ var division = function(a, b){
 var modulo = function(a, b){
     return (a % b);
 };
+var Invincible2 = ["Micheal", "Albert"];
+console.log(Invincible2); 
+Invincible2.push("Mutesasira");
+console.log(Invincible2);
+console.log(add(2 , 3)); 
+console.log(multiplication(2 , 3)); 
+console.log(division(2 , 3)) 
+console.log(subtract(2 , 3)) 
+console.log(modulo(2 , 3)) 
